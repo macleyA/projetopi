99eb66d466903519194baf9027983f3fe4842aa8
macleyA <macleyalexandre@gmail.com>
2024-10-30 16:42:46 -0300

    Configurando classe de modelo

--- src/main/java/Ifrn/pi/eventos/models/Evento.java
+++ src/main/java/Ifrn/pi/eventos/models/Evento.java
@@ -1,42 +1,36 @@
 package Ifrn.pi.eventos.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Evento {
-	private String nome;
-	private String local;
-	private String data;
-	private String horario;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+    private String nome;
+    private String local;
+    private String data;
+    private String horario;
 
-	public String getNome() {
-		return nome;
-	}
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public String getLocal() { return local; }
+    public void setLocal(String local) { this.local = local; }
 
-	public String getLocal() {
-		return local;
-	}
+    public String getData() { return data; }
+    public void setData(String data) { this.data = data; }
 
-	public void setLocal(String local) {
-		this.local = local;
-	}
-
-	public String getData() {
-		return data;
-	}
-
-	public void setData(String data) {
-		this.data = data;
-	}
-
-	public String getHorario() {
-		return horario;
-	}
-
-	public void setHorario(String horario) {
-		this.horario = horario;
-	}
+    public String getHorario() { return horario; }
+    public void setHorario(String horario) { this.horario = horario; }
     
-    
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
 }
