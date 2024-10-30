4ede59cbb07f0a88a23a378a9b5789930bab137f
macleyA <macleyalexandre@gmail.com>
2024-10-30 16:43:59 -0300

    Configurando banco de dados

--- src/main/java/Ifrn/pi/eventos/Controllers/EventosController.java
+++ src/main/java/Ifrn/pi/eventos/Controllers/EventosController.java
@@ -2,14 +2,25 @@ package Ifrn.pi.eventos.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import Ifrn.pi.eventos.models.Evento;
 
 @Controller
-
 public class EventosController {
-	
-	@RequestMapping("/eventos/form")
-	public String form() {
-		return "formEvento";
-	}
+
+    @RequestMapping("/eventos/form")
+    public String form() {
+        return "formEvento";
+    }
+
+    @RequestMapping(value = "/eventos", method = RequestMethod.POST)
+    public String receberFormulario(Evento evento) {
+        System.out.println("Nome: " + evento.getNome());
+        System.out.println("Local: " + evento.getLocal());
+        System.out.println("Data: " + evento.getData());
+        System.out.println("Horário: " + evento.getHorario());
+        return "confirmacao";
+    }
+
 
 }
